a3bd0f80cebe617c2fc9447014c728d908c66952
Isaac-Jimenez-B <isaacalonsojimenezb@gmail.com>
2026-05-06 19:34:47 -0600

    Agrega datos de prueba y mejora lectura de archivos para validar persona 4

--- app/FileWrite.hs
+++ app/FileWrite.hs
@@ -1,6 +1,9 @@
 module FileWrite where
+
 import Types
 import System.IO
+import Data.Char (isSpace)
+import Text.Read (readMaybe)
 
 rutaRegistros :: FilePath
 rutaRegistros = "app/data/registros.txt"
@@ -11,29 +14,32 @@ rutaReglas = "app/data/reglas.txt"
 rutaPresupuestos :: FilePath
 rutaPresupuestos = "app/data/presupuestos.txt"
 
+limpiarLinea :: String -> String
+limpiarLinea = quitarEspaciosFinales . quitarEspaciosIniciales
+  where
+    quitarEspaciosIniciales = dropWhile isSpace
+    quitarEspaciosFinales = reverse . dropWhile isSpace . reverse
+
+lineasValidas :: String -> [String]
+lineasValidas contenido =
+    filter (not . null) (map limpiarLinea (lines contenido))
+
+leerListaDesdeArchivo :: Read a => FilePath -> IO [a]
+leerListaDesdeArchivo ruta = do
+    contenido <- readFile ruta
+    let ls = lineasValidas contenido
+    case traverse readMaybe ls of
+        Just xs -> return xs
+        Nothing -> error ("Error al leer datos en el archivo: " ++ ruta)
+
 cargarRegistros :: IO [RegistroFinanciero]
-cargarRegistros = do
-    contents <- readFile rutaRegistros
-    let ls = lines contents
-    if null ls
-       then return []
-       else return (map read ls)
+cargarRegistros = leerListaDesdeArchivo rutaRegistros
 
 cargarReglas :: IO [Regla]
-cargarReglas = do
-    contents <- readFile rutaReglas
-    let ls = lines contents
-    if null ls
-       then return []
-       else return (map read ls)
+cargarReglas = leerListaDesdeArchivo rutaReglas
 
 cargarPresupuestos :: IO [Presupuesto]
-cargarPresupuestos = do
-    contents <- readFile rutaPresupuestos
-    let ls = lines contents
-    if null ls
-       then return []
-       else return (map read ls)
+cargarPresupuestos = leerListaDesdeArchivo rutaPresupuestos
 
 guardarRegistros :: [RegistroFinanciero] -> IO ()
 guardarRegistros registros =
